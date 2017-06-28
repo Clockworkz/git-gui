9bdeff40b2a2a18dd24a090fcdaffecc127ab8f6
mwagh-9823 <mwagh@simplifydigital.co.uk>
2017-06-28 22:33:59 +0100

    inital commit

---
+++ app/main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow} = require('electron')
+const url = require("url");
+const path = require("path");
+let win;
+
+createWindow = () => {
+    win = new BrowserWindow({width: 800, height: 600});
+    win.setTitle("Git GUI");
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, './web/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.on('closed', () => {
+        win = null;
+    });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
